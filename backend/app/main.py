276d888c8023bbcc41c89fa98487118825e95239
Lollo003 <150928506+Lollo003@users.noreply.github.com>
2023-12-16 10:19:11 +0100

    other graphical changes

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,21 +1,28 @@
+"""
+Backend module for the FastAPI application.
 
+This module defines a FastAPI application that serves
+as the backend for the project.
+"""
 
 from fastapi import FastAPI,HTTPException, Request
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
 from fastapi.responses import JSONResponse
+from datetime import datetime
 import pandas as pd
 import sys
 
-app = FastAPI()
+
+
 sys.path.append('app')
 
-# Load flight data and integrate cleaning functions
+
 from mymodules.Cleaning import flights_data_cleaned
 from mymodules.df_integrations import flights
-from mymodules.Feature_1_avg_price import calculate_average_price, filter_destinations
-from mymodules.Avg_Class_Price import calculate_average_price_airline
 from mymodules.Destination_random import randomize_destination
+from mymodules.Avg_Class_Price import calculate_average_price_airline
+
 
 
 
@@ -31,7 +38,7 @@ def read_root():
     """
     return {"Hello": "World"}
 
-@app.get("/query/{departure}")
+@app.get("/random/{departure}")
 def randomize_destination_endpoint(departure: str):
     result = randomize_destination(departure, flights)
     return result
@@ -46,16 +53,10 @@ def airlines():
     tt = flights_data_cleaned['Air Carrier'].drop_duplicates().to_json(orient='records')
     return tt
 
-
 @app.get('/{AIRLINES}')
 def average_web(AIRLINES):
     result = calculate_average_price_airline(flights, AIRLINES)
     return result
 
 
-@app.get('/{Departure}/{Arrival}')
-def avg_price(Departure:str, Arrival:str):
-    result = calculate_average_price(flights, Departure, Arrival)
-    result = round(result,2)
-    result = "{:.2f}".format(result)
-    return result
+
