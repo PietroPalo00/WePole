b8c6e7be0adc80455f7833d0d18209a5d1bf2a1e
Lollo003 <150928506+Lollo003@users.noreply.github.com>
2023-12-17 15:34:16 +0100

    Update Destination_random.py

--- backend/app/mymodules/Destination_random.py
+++ backend/app/mymodules/Destination_random.py
@@ -7,37 +7,40 @@ import random
 import difflib
 
 def randomize_destination(departure, df):
-    '''
-    parameters:
-    - departure -> the airport desidered by the user, should be chosen from a list in the frontend
-    - df -> in our case "flights", should work with analog df with analog values
-    Returns:
-    - string with {random-destination} and {available dates}'''
+    """
+    Generate a list of possible destinations and available dates based on the departure airport.
 
+    Parameters:
+    - departure (str): The user's selected departure airport.
+    - df (DataFrame): The flight data, such as "flights" DataFrame.
+
+    Returns:
+    - dict: A dictionary containing "give_output" and "arrivalcity".
+    """
     departure = departure.upper()
 
-    if df.empty == True:
-        return f'data are not available for this dataset'
+    if df.empty:
+        return "Data are not available for this dataset"
     else:
         possible_destinations = df[df['Departure'] == departure]['Arrival'].unique()
 
         if possible_destinations.size > 0:
             chosen_destination = random.choice(possible_destinations)
 
-        def give_output(chosen_destination, df):
-            if chosen_destination:    
-                available_dates = df[(df['Departure'] == departure) & (df['Arrival'] == chosen_destination)]['Travel Date']
+            def give_output(destination, df):
+                available_dates = df[(df['Departure'] == departure) & (df['Arrival'] == destination)]['Travel Date']
                 available_dates = ', '.join(available_dates.astype(str))
-                return f'{chosen_destination}', f'AVAILABLE DATES: {available_dates}'
-            else:
-                return "We are sorry, but we don't have any flights from this departure"
+                return f'{destination}', f'AVAILABLE DATES: {available_dates}'
+        else:
+            return "We are sorry, but we don't have any flights from this departure"
+
     def arrivalcity():
         return chosen_destination
+
     return {
         'give_output': give_output(chosen_destination, df),
         'arrivalcity': arrivalcity()
     }
-
 
 
 
