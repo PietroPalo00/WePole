c3b8078d694cd17c3864154b9aec40390238b365
PietroPalo00 <890743@stud.unive.it>
2023-12-17 23:11:13 +0100

    deleted additional and useless files and adapt main to pep8

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,16 +1,11 @@
 
 
-from fastapi import FastAPI,HTTPException, Request
+from fastapi import FastAPI, HTTPException, Request
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
 from fastapi.responses import JSONResponse
 import pandas as pd
 import sys
-
-app = FastAPI()
-sys.path.append('app')
-
-# Load flight data and integrate cleaning functions
 from mymodules.cleaning import df_clean
 from mymodules.df_integrations import flights
 from mymodules.feat_1_avg_price import calculate_average_price
@@ -20,6 +15,7 @@ from mymodules.feat_4_cheapest import cheapest_to_fly
 from mymodules.cleaning import clean_cost
 
 app = FastAPI()
+sys.path.append('app')
 
 
 @app.get('/')
@@ -45,7 +41,7 @@ def combined_endpoint(departure: str):
 def get_departure_from_csv():
     results = flights['Departure'].drop_duplicates().to_json(orient='records')
     return results
-    
+
 
 @app.get('/get_airline')
 def airlines():
@@ -60,15 +56,15 @@ def average_web(AIRLINES):
 
 
 @app.get('/avg/{Departure}/{Arrival}')
-def avg_price(Departure:str, Arrival:str):
+def avg_price(Departure: str, Arrival: str):
     result = calculate_average_price(flights, Departure, Arrival)
-    if result != None:
-        result = round(result,2)
+    if result is not None:
+        result = round(result, 2)
         result = "{:.2f}".format(result)
         return result
     else:
         return None
-  
+
 
 @app.get('/arrival-{Arrival}')
 def cheapest(Arrival):
